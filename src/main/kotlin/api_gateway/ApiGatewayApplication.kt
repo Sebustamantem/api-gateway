60178cd46b649fb9b0c2f7557b49512cfa298a95
Sebastian Bustamante <Se.bustamantem@duocuc.cl>
2025-11-30 01:39:24 -0300

    configurando

--- src/main/kotlin/api_gateway/ApiGatewayApplication.kt
+++ src/main/kotlin/api_gateway/ApiGatewayApplication.kt
@@ -2,10 +2,8 @@ package com.example.apigateway
 
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.runApplication
-import org.springframework.cloud.client.discovery.EnableDiscoveryClient
 
 @SpringBootApplication
-@EnableDiscoveryClient
 class ApiGatewayApplication
 
 fun main(args: Array<String>) {
